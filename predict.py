ee8b5004dc67773e9d5bd2f2943d0cab061e42c3
Natasha Estrada <nestra5@wgu.edu>
2025-11-29 13:28:35 -0500

    Finalized Image_Classifier_Project.ipynb

---
+++ predict.py
@@ -0,0 +1,13 @@
+import argparse
+import json
+import torch
+from torchvision import models
+import numpy as np
+from PIL import Image
+import torch.nn.functional as F
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
