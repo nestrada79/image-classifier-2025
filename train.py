61dbaafd55584783b52cae2634998ee7dc837151
Natasha Estrada <nestra5@wgu.edu>
2025-11-29 21:40:41 -0500

    Started train.py

---
+++ train.py
@@ -0,0 +1,13 @@
+import argparse
+import json
+import torch
+from torchvision import datasets, transforms, models
+from torch import nn, optim
+import os
+
+def main():
+    # Will be filled in step-by-step
+    pass
+
+if __name__ == "__main__":
+    main()
